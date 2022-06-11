b3dfe1a46833fca5014531b1b88335bada052b67
Ismail Enis Yilmaz <yilmaz.ismailenis@gmail.com>
2022-06-11 18:26:06 +0300

    C#_Angular_Kamp_4.GunSonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }//ürünün birim fiyatı
+        public int UnitsInStock { get; set; }//stok
+
+        //CRUD / Create - Read - Update - Delete / ekle - oku - güncelle - sil
+    }
+}
