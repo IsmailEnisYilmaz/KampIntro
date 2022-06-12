52a65846aebe426bd9c8af9bda23839c1d604746
Ismail Enis Yilmaz <yilmaz.ismailenis@gmail.com>
2022-06-12 13:46:09 +0300

    C#_Angular_Kampı_4.GunOdevi

---
+++ GenericMyDictionary4.GunOdev/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericMyDictionary4.GunOdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> kullaniciIdVeAdlar = new MyDictionary<int,string>();
+            Console.WriteLine("Başlangıç Uzunluğu: " + kullaniciIdVeAdlar.Length);
+            kullaniciIdVeAdlar.Add(1, "Enis");
+            kullaniciIdVeAdlar.Add(2, "Leyla");
+            kullaniciIdVeAdlar.Add(3, "Mustafa");
+            kullaniciIdVeAdlar.Add(4, "Lale");
+
+
+            for (int i = 0; i < kullaniciIdVeAdlar.Length; i++)
+            {
+                Console.WriteLine("ID: "+kullaniciIdVeAdlar.Keys[i] + ", Ad: " + kullaniciIdVeAdlar.Values[i]);
+            }
+            Console.WriteLine("Bitiş Uzunluğu: " + kullaniciIdVeAdlar.Length);
+        }
+    }
+}
